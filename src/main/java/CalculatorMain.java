d15d6b7a1b1ffadbdce3868290c9c0d9148c70e1
Rishabh Batra <csecec.1803195@gmail.com>
2025-07-05 15:26:56 +0530

    More methods

--- src/main/java/CalculatorMain.java
+++ src/main/java/CalculatorMain.java
@@ -2,13 +2,19 @@ class Calculator{
     public int add(int a, int b) {
         return a + b;
     }
+    public int subtract(int a, int b) {
+        return a - b;
+    }
 }
 public class CalculatorMain {
     public static void main(String[] args) {
 
         Calculator cal = new Calculator();
         int add = cal.add(1, 2);
-        System.out.println("Result : " + add);
+        System.out.println("Addition Result : " + add);
+
+        int subtract = cal.subtract(5, 6);
+        System.out.println("Subtraction Result : " + subtract);
 
     }
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -9,4 +9,11 @@ public class CalculatorTest {
         int result = calculator.add(1, 2);
         assertEquals(3, result, "1 + 2 should equal 3");
     }
+
+    @Test
+    void testSubtract() {
+        Calculator calculator = new Calculator();
+        int result = calculator.subtract(5, 2);
+        assertEquals(3, result, "5-2 should equal 3");
+    }
 }
