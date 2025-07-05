d5d08a0dc384ae31685514c72ade3e4bba4481a5
Rishabh Batra <csecec.1803195@gmail.com>
2025-07-05 16:02:02 +0530

    ngrok credential

--- src/main/java/CalculatorMain.java
+++ src/main/java/CalculatorMain.java
@@ -15,6 +15,7 @@ public class CalculatorMain {
 
         System.out.println();
 //https://6bc5-2401-4900-8831-731-6430-3a3b-33b6-99c6.ngrok-free.app/github-webhook/
+        // ngrok config add-authtoken 2zS2gXTrs0EVi7ESl9eTzNNUjNv_3i23h2dvAtJ7i8wxQ9Fe8
 
         int subtract = cal.subtract(5, 6);
         System.out.println("Subtraction Result : " + subtract);
